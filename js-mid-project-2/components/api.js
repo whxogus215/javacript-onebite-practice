5de1904b6e18abc0c2b26b7babb90a8122ab2075
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-04-05 17:42:58 +0900

    feat: 모듈 시스템을 활용해 컴포넌트로 분리한 프로젝트트

---
+++ js-mid-project-2/components/api.js
@@ -0,0 +1,13 @@
+const API_URL = "https://animal-api-two.vercel.app/";
+
+export const request = async () => {
+  let res = await fetch(API_URL);
+  try {
+    if (res) {
+      let data = await res.json();
+      return data.photos;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
---
+++ js-mid-project-2/src/index.js
@@ -0,0 +1,6 @@
+import App from "../App.js";
+
+const $app = document.getElementById("app");
+
+// App 모듈 인스턴스가 가져온 요소를 조작하도록 한다.
+new App($app);
