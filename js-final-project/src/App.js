770c72d13ffc0245dbd010a5ade3436d09d199fd
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-04-10 13:10:48 +0900

    feat: 최종 프로젝트 - 지역 리스트 컴포넌트 구현

--- js-final-project/src/App.js
+++ js-final-project/src/App.js
@@ -74,7 +74,22 @@ export default function App($app) {
       });
     },
   });
-  const regionList = new RegionList();
+  const regionList = new RegionList({
+    $app,
+    initialState: this.state.region,
+    handleRegion: async (region) => {
+      history.pushState(null, null, `/${region}?sort=total`);
+      const cities = await request(0, region, "total");
+      this.setState({
+        ...this.state,
+        startIdx: 0,
+        sortBy: "total",
+        region: region,
+        searchWord: "",
+        cities: cities,
+      });
+    },
+  });
   const cityList = new CityList({
     $app,
     initialState: this.state.cities,
@@ -103,8 +118,33 @@ export default function App($app) {
       sortBy: this.state.sortBy,
       searchWord: this.state.searchWord,
     });
+    regionList.setState(this.state.region);
     cityList.setState(this.state.cities);
   };
+
+  window.addEventListener("popstate", async () => {
+    const urlPath = window.location.pathname;
+
+    const prevRegion = urlPath.replace("/", "");
+    const prevSortBy = getSortBy();
+    const prevSearchWord = getSearchWord();
+    const prevStartIdx = 0;
+    const prevCities = await request(
+      prevStartIdx,
+      prevRegion,
+      prevSortBy,
+      prevSearchWord
+    );
+
+    this.setState({
+      ...this.state,
+      startIdx: prevStartIdx,
+      sortBy: prevSortBy,
+      region: prevRegion,
+      searchWord: prevSearchWord,
+      cities: prevCities,
+    });
+  });
 
   const init = async () => {
     const cities = await request(
